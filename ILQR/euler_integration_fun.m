781e5ca51398b044d2f25d2e7a0cc0d6a3d99a2e
Simone Rotondi <rotondi97simone@gmai.com>
2022-05-07 14:39:04 +0200

    change integration to right size

--- ILQR/euler_integration_fun.m
+++ ILQR/euler_integration_fun.m
@@ -1,5 +1,13 @@
-function new_state = euler_integration_fun(state,dy,dt)
-%integrate as euler
-new_state=state+dy*dt;
-end
+function new_state = euler_integration_fun(state, dy, dt)
+    size(state)
+    %integrate as euler
+    %%test with single value
+    %new_state=state+dy*dt;
+    %%test for inverted pendulum
+    new_state(2) = state(2) + dy(2) * dt;
+    new_state(1) = state(1) + new_state(2) * dt;
 
+    new_state(4) = state(4) + dy(4) * dt;
+    new_state(3) = state(3) + new_state(4) * dt;
+    new_state=new_state.';
+end
