38178da9adadd9586071162fb0c2b5dcc575b552
Simone Rotondi <rotondi97simone@gmai.com>
2022-05-15 10:24:54 +0200

    ilqr

---
+++ ILQR/iLQR_DDP_function.m
@@ -0,0 +1,163 @@
+function next_single_control = iLQR_DDP_function(istate, state_d, it)
+    %global  u new_u Gxx Gxu Guu Gux gx gu d L A_ A B_ B N n
+    global u %use global u to remember last optimized values
+    next_single_control = 0; %default next control if we have a nan value
+    dt = 0.01; %delta t for integration
+
+    [n_states, sz] = size(state_d);
+    n_controls = 1;
+    Q = eye(n_states) * 1;
+    Q(1, 1) = 10;
+    Q(3, 3) = 10;
+    R = 0.0001;
+
+    horizon = 4; %time S
+    horizon_disc = floor(horizon / dt);
+
+    if horizon_disc > (sz)
+        %horizon = (sz) * dt;
+        horizon_disc = (sz);
+    end
+
+    state_d = state_d(:, 1:horizon_disc);
+
+    S = zeros(n_states, horizon_disc * n_states);
+    s = zeros(n_states, horizon_disc);
+
+    L = ones(n_controls, (horizon_disc - 1) * n_states); %size depends both from the number of controls and states
+    d = ones(n_controls, (horizon_disc - 1)); % size depends from the number of controls
+
+    if exist("u", "var")
+        [usz, ~] = size(u);
+        u = [0, u];
+        u = [u(3:end), zeros(1, horizon_disc - usz)];
+    else
+        u = ones(horizon_disc - 1, 1) * (0);
+    end
+
+    state_array = zeros(n_states, horizon_disc);
+    state_array(:, 1) = istate;
+    time_array = zeros(1, horizon_disc);
+    time_array(1, 1) = it;
+    t = it; %inital t
+
+    for elem = 1:horizon_disc - 1 %compute the forward dynamics to define the error
+        %dy = ForwardDynamics(state_array(:, elem), u(elem));
+        %state_array(:, elem + 1) = euler_integration_fun(state_array(:, elem), dy, dt);
+        state_array(:, elem + 1) = dynamics_rk4(state_array(:, elem), u(elem), dt);
+        t = t + dt;
+        time_array(1, elem + 1) = t;
+    end
+
+    tiledlayout(2, 1);
+    nexttile
+    plot(time_array, [state_array(1, :); state_array(3, :)])
+    legend("x", "phi")
+    nexttile
+    plot(time_array, [state_array(2, :); state_array(4, :)])
+    legend("dx", "dphi")
+
+    %cost = fullcost(state_array, u, state_d, Q, R);
+
+    while (max(abs(d(:, :))) > 1e-2)
+
+        S(:, horizon_disc * 4 - 3:horizon_disc * 4) = Q * 1000;
+        s(:, horizon_disc) = Q * 1000 * (state_array(:, horizon_disc) - state_d(:, horizon_disc));
+
+        A_ = zeros(n_states, (horizon_disc - 1) * n_states);
+        B_ = zeros(n_states, horizon_disc - 1);
+
+        for n = (horizon_disc - 1):-1:1
+            N = (4 * n - 3):(4 * n);
+
+            %derivatives
+            q = Q * (state_array(:, n) - state_d(:, n));
+            r = R * u(n);
+            [A_(:, N), B_(:, n)] = linearization_discretization(u(n), state_array(:, n), 1);
+
+            A = A_(:, N);
+            B = B_(:, n);
+
+            gx = q + A.' * s(:, n + 1);
+            gu = r + B.' * s(:, n + 1);
+
+            Gxx = Q + A.' * S(:, N + 4) * A;
+            Guu = R + B.' * S(:, N + 4) * B; %H
+
+            Gxu = A.' * S(:, N + 4) * B;
+            Gux = B.' * S(:, N + 4) * A;
+
+            d(:, n) = pinv(Guu) * gu; %d
+            L(:, N) = pinv(Guu) * Gux; %K
+
+            s(:, n) = gx - L(:, N).' * gu + L(:, N).' * Guu * d(:, n) - Gxu * d(:, n);
+            S(:, N) = Gxx + L(:, N).' * Guu * L(:, N) - Gxu * L(:, N) - L(:, N).' * Gux;
+
+            % dJ = dJ + gu' * d(:, n);
+
+        end
+
+        new_state_array = zeros(n_states, horizon_disc);
+        new_state_array(:, 1) = istate;
+        new_u = zeros(n_controls, horizon_disc - 1);
+
+        %forward iteration
+        for n = 1:horizon_disc - 1
+            N = (4 * n - 3):(4 * n); %Prendere elemento da n*4-3 a elemento n*4
+
+            new_u(n) = u(n) - d(:, n) - L(:, N) * (new_state_array(:, n) - state_array(:, n));
+
+            %dy = ForwardDynamics(new_state_array(:, n), new_u(n));
+            %new_state_array(:, n + 1) = euler_integration_fun(new_state_array(:, n), dy, dt);
+            new_state_array(:, n + 1) = dynamics_rk4(new_state_array(:, n), new_u(n), dt);
+        end
+
+        %cost = fullcost(new_state_array, new_u, state_d, Q, R);
+
+        state_array = new_state_array;
+        u = new_u;
+
+        tiledlayout(2, 1);
+        nexttile
+        plot(time_array, [state_array(1, :); state_array(3, :)])
+        legend("x", "phi")
+        nexttile
+        plot(time_array, [state_array(2, :); state_array(4, :)])
+        legend("dx", "dphi")
+        pause(0.001)
+
+        next_single_control = u(1);
+
+        %max(abs(d(:, :)))
+    end
+
+end
+
+function c = stage_cost(x, u, x_d, Q, R)
+    c = 0.5 * (x - x_d).' * Q * (x - x_d) + 0.5 * u.' * R * u;
+end
+
+function c = terminal_cost(x, x_d, Q)
+    Qn = Q * 1000;
+    c = 0.5 * (x - x_d).' * Qn * (x - x_d);
+end
+
+function J = fullcost(xtraj, utraj, x_d, Q, R)
+    J = 0;
+    [~, hor] = size(xtraj);
+
+    for k = 1:hor - 1
+        J = J + stage_cost(xtraj(:, k), utraj(k), x_d(:, k), Q, R);
+    end
+
+    k = hor;
+    J = J + terminal_cost(xtraj(:, k), x_d(:, k), Q);
+end
+
+function state = dynamics_rk4(state, u, dt)
+    f1 = ForwardDynamics(state, u);
+    f2 = ForwardDynamics(state + 0.5 * dt * f1, u);
+    f3 = ForwardDynamics(state + 0.5 * dt * f2, u);
+    f4 = ForwardDynamics(state + dt * f3, u);
+    state = state + (dt / 6) * (f1 + 2 * f2 + 2 * f3 + f4);
+end
--- ILQR/main.m
+++ ILQR/main.m
@@ -3,11 +3,13 @@ function main(Q, R, wclose)
     if nargin < 3
         wclose = 0;
     end
+
     if nargin < 2
-        R=0.0001
+        R = 0.0001;
     end
-    if nargin <1
-        Q=diag([10,1,100,1])
+
+    if nargin < 1
+        Q = diag([10, 1, 100, 1]);
     end
 
     if (wclose)
@@ -15,36 +17,52 @@ function main(Q, R, wclose)
     end
 
     clc;
-
-    y = [0.1; 0; pi-0.1; 0]; %initial point
+    u = 0;
+    y = [0; 0; 0; 0]; %initial point
     t = 0.01; %initial time
-    tf = 3; %final time
+    tf = 10; %final time
     dt = 0.01; %increasing time %time step
 
     time = t:dt:tf; %time array
     %traj_d=-0.06*(time-1.7).^5+0.6; %desired trajectory
-    traj_d = repmat([0; 0; pi; 0], [1, tf / dt]);
-    state_array = []; %array degli stati
+    traj_d = repmat([1; 0; pi; 0], [1, tf / dt]);
+    %traj_d(1,:)=-0.06*(time-1.7).^5+0.6; %desired trajectory
+    state_array = [y]; %array degli stati
     control_array = []; %array del controllo
-    time_array = []; %array del tempo (dovrebbe coincidere con la l'array "time")
-    y_d_array = []; %array della traiettoria (dovrebbe coincidere con la l'array "traj_d")
+    time_array = [t]; %array del tempo (dovrebbe coincidere con la l'array "time")
+    y_d_array = [0; 0; pi; 0]; %array della traiettoria (dovrebbe coincidere con la l'array "traj_d")
+
+    rk4_state_array = [y];
+    linearized__state_array = [y];
 
     while t < tf %process start
 
         %find u control
         y_des = traj_d(:, floor(t / dt));
-        u=iLQR_function(y,traj_d(:,floor(t/dt):end),t);
+        %u=iLQR_function(y,traj_d(:,floor(t/dt):end),t);
+        u = iLQR_DDP_function(y, traj_d(:, floor(t / dt):end), t);
         %u = LQR_function(y, y_des, Q, R);
         %save to plot
         control_array = [control_array, u];
-        time_array = [time_array, t];
-        y_d_array = [y_d_array, y_des];
-        state_array = [state_array, y];
 
         %compute dynamics
+
+        y_d_array = [y_d_array, y_des];
+
         dy = ForwardDynamics(y, u);
-        y = euler_integration_fun(y, dy, dt); % start to y and use a dy increment for delta t (euler integration)
+        y = euler_integration_fun(y, dy, dt);
+        state_array = [state_array, y];
+
+        %     [A,B]=linearization_discretization(u,rk4_state_array(:,end),1);
+        %     linearized__state_array =[linearized__state_array, A*rk4_state_array(:,end)+B*u];
+        %
+        %
+        %     rk4_state_array = [rk4_state_array,dynamics_rk4(rk4_state_array(:,end),u,dt)];
+        %
+
         t = t + dt; %time increment
+        time_array = [time_array, t];
+
     end
 
     save('mainVars.mat') % save variables to
@@ -55,10 +73,36 @@ function main(Q, R, wclose)
     plot(time_array, y_d_array)
     legend("x", "dx", "phi", "dphi", "d-x", "d-dx", "d-phi", "d-dphi")
     title("trajectory and desired trajectory")
+
+    % nexttile
+    % plot(time_array, rk4_state_array)
+    % hold on
+    % plot(time_array, y_d_array)
+    % legend("x", "dx", "phi", "dphi", "d-x", "d-dx", "d-phi", "d-dphi")
+    % title("trajectory and desired trajectory")
+
+    % nexttile
+    % plot(time_array, linearized__state_array)
+    % hold on
+    % plot(time_array, y_d_array)
+    % legend("x", "dx", "phi", "dphi", "d-x", "d-dx", "d-phi", "d-dphi")
+    % title("trajectory and desired trajectory")
+
     nexttile
     plot(time_array, y_d_array - state_array)
     legend("x", "dx", "phi", "dphi")
     title("error trajectory")
+
     nexttile
-    plot(time_array, control_array)
+    plot(time_array, [control_array, 0])
     title("controls")
+
+end
+
+function state = dynamics_rk4(state, u, dt)
+    f1 = ForwardDynamics(state, u);
+    f2 = ForwardDynamics(state + 0.5 * dt * f1, u);
+    f3 = ForwardDynamics(state + 0.5 * dt * f2, u);
+    f4 = ForwardDynamics(state + dt * f3, u);
+    state = state + (dt / 6) * (f1 + 2 * f2 + 2 * f3 + f4);
+end
