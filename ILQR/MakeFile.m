1dcd78b15361e6785bdf2fd858daae06e81d72dd
Simone Rotondi <rotondi97simone@gmai.com>
2022-07-19 11:22:07 +0200

    makefile

--- ILQR/MakeFile.m
+++ ILQR/MakeFile.m
@@ -9,5 +9,9 @@ Q=diag([1000,10,0,10,10,1]);
 R=diag([0.01,0.01]);
 Qn=diag([1000,10,0,10,10,1])*10;
 Twip_main(Q,R,Qn,1)
+% or just Twip_main()
+pause
+Disp("press enter to clear the Command Window and compile")
+pause
 codegen Twip_main -config cfg -args {Q,R,Qn,1}
 Twip_main_mex(Q,R,Qn,1)
