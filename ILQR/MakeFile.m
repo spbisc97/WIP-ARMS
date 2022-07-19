6d3166cc09f21685c6ed020df87429d6c43a86ef
Simone Rotondi <rotondi97simone@gmai.com>
2022-07-19 10:41:28 +0200

    MakeFileUpdate

--- ILQR/MakeFile.m
+++ ILQR/MakeFile.m
@@ -5,9 +5,9 @@ cfg.EnableAutoParallelization=1;
 cfg.EnableImplicitExpansion=1;
 %cfg.ExtrinsicCalls=true;
 %cfg.EnableAutoParallelizationReporting=1;
-Q=diag([10,1,10,1,10,1]);
+Q=diag([1000,10,0,10,10,1]);
 R=diag([0.01,0.01]);
-Qn=diag([10,1,10,1,10,1])*1000;
-
+Qn=diag([1000,10,0,10,10,1])*10;
+Twip_main(Q,R,Qn,1)
 codegen Twip_main -config cfg -args {Q,R,Qn,1}
 Twip_main_mex(Q,R,Qn,1)
